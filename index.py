13af8eb00fdf10b0e49431842f72a777c4af9635
AWS2-05 <mcolominasrojas@iesesteveterradas.cat>
2018-01-11 18:00:48 +0100

    segundo comit

--- index.py
+++ index.py
@@ -1,25 +1,25 @@
 from flask import Flask, render_template, request, session
-from flask.ext.session import Session
 from random import randint
 
 app = Flask(__name__)
 app.secret_key = 'superSecretKey'
 
 @app.route('/', methods=['GET', 'POST'])
-def hello():
+def main():
 	if request.method == 'POST':
 		num = int(request.form["numero"])
 		rand = getRandom()
 		if(rand == None):
 			generarRandom()
+			rand = getRandom()
 
 		if(num > rand):
-			return render_template('index.html', msg="El numero introducido es mas grande")
+			return render_template('index.html', msg="<p>El numero introducido es mas grande.</p>")
 		elif(num < rand):
-			return render_template('index.html', msg="El numero introducido es mas pequeno")
+			return render_template('index.html', msg="<p>El numero introducido es mas pequeno.</p>")
 		else:
-			resetRandom()
-			return render_template('index.html', msg="Has ganado!!")
+			generarRandom()
+			return render_template('index.html', msg="<h2>Has ganado!!</h2><p>Se a generado otro numero para que vuelvas a jugar.</p>")
 	else:
 		return render_template('index.html')
 
@@ -27,11 +27,4 @@ def generarRandom():
 	session['rand'] = randint(1, 100)
 
 def getRandom():
-	return session.get("rand", None)
-
-def removeRandom():
-	session['rand'] = None
-
-def resetRandom():
-	removeRandom()
-	generarRandom()
+	return session.get('rand', None)
